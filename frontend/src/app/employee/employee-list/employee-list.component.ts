d287100a03d09aa7042fc4cd2acb7c78c60f286d
gourab-sinha <gourab19964u@gmail.com>
2020-12-25 16:40:07 +0530

    event handling added using observer

--- frontend/src/app/employee/employee-list/employee-list.component.ts
+++ frontend/src/app/employee/employee-list/employee-list.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Employee } from '../employee.model';
 import { EmployeeService } from '../employee.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
-export class EmployeeListComponent implements OnInit {
+export class EmployeeListComponent implements OnInit, OnDestroy {
   employees: Employee[] = [];
+  private employeesSubs: Subscription;
   constructor(public employeeService: EmployeeService) { }
 
   ngOnInit(): void {
     this.employees = this.employeeService.getEmployees();
+    this.employeesSubs = this.employeeService.getEmployeeUpdateListener().subscribe((employees: Employee[])=>{
+      this.employees = employees;
+    });
     console.log(this.employees);
+  }
+
+  ngOnDestroy(): void{
+    // prevents memory leaks
+    this.employeesSubs.unsubscribe();
   }
 
 }
--- frontend/src/app/employee/employee.service.ts
+++ frontend/src/app/employee/employee.service.ts
@@ -1,23 +1,24 @@
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { Employee } from "./employee.model";
+import { Subject } from 'rxjs';
 
 @Injectable({providedIn: "root"})
 export class EmployeeService{
-    private employees: Employee[] = [
-        {firstName: 'Gourab', lastName: 'Sinha', role: 'Software Developer', email: 'gourab@g.com', status: true},
-        {firstName: 'Gourab', lastName: 'Sinha', role: 'Software Developer', email: 'gourab@g.com', status: true},
-        {firstName: 'Gourab', lastName: 'Sinha', role: 'Software Developer', email: 'gourab@g.com', status: true},
-        {firstName: 'Gourab', lastName: 'Sinha', role: 'Software Developer', email: 'gourab@g.com', status: true},
-        {firstName: 'Gourab', lastName: 'Sinha', role: 'Software Developer', email: 'gourab@g.com', status: true},
-    ];
+    private employees: Employee[] = [];
+    private employeesUpdated = new Subject<Employee[]>();
     constructor(private router: Router){}
     getEmployees(){
         return [...this.employees];
     }
 
+    getEmployeeUpdateListener(){
+        return this.employeesUpdated.asObservable();
+    }
+
     addEmployee(employee: Employee){
         this.employees.push(employee);
+        this.employeesUpdated.next([...this.employees]);
         this.router.navigate(["/"]);
     }
 }
