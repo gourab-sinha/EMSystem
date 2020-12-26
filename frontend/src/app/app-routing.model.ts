fadad141ef801502412c7e366a61187ac91d185b
gourab-sinha <gourab19964u@gmail.com>
2020-12-26 23:42:46 +0530

    Signup form added

--- frontend/src/app/app-routing.model.ts
+++ frontend/src/app/app-routing.model.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./auth/login/login.component";
+import { SignupComponent } from "./auth/signup/signup.component";
 import { EmployeeCreateComponent } from "./employee/employee-create/employee-create.component";
 import { EmployeeListComponent } from "./employee/employee-list/employee-list.component";
 
@@ -8,7 +9,8 @@ const routes: Routes = [
     { path: '', component: EmployeeListComponent},
     { path: 'create', component: EmployeeCreateComponent},
     { path: 'edit/:employeeId', component: EmployeeCreateComponent},
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent}
 
 ];
 @NgModule({
