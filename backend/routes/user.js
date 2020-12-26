d080e3084e98950f89c840607b88963ed8cb96b2
gourab-sinha <gourab19964u@gmail.com>
2020-12-27 00:46:32 +0530

    login functionality addded

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+const user = require('../models/user');
 
 router.post("/signup", (req,res,next)=>{
     console.log("Inside");
@@ -21,8 +23,38 @@ router.post("/signup", (req,res,next)=>{
                 error: err
             });
         });
-    })
-    
+    }); 
 });
 
+
+router.post("/login", (req,res,next)=>{
+    let fetchedUser;
+    User.findOne({email: req.body.email}).then(user=>{
+        if(!user){
+            return res.status(401).json({
+                message: "Auth failed"
+            });
+        }
+        this.fetchedUser = user;
+        return bcrypt.compare(req.body.password, user.password);
+    }).then(result =>{
+        if(!result){
+            return res.status(401).json({
+                message: "Auth failed"
+            });
+        }
+        const token = jwt.sign({
+            email: this.fetchedUser.email, 
+            userId: this.fetchedUser._id,
+        }, "secret_this_should_be_longer", {expiresIn: "1h"});
+
+        res.status(200).json({
+            token: token
+        });
+    }).catch(err=>{
+        res.status(401).json({
+            message: "Auth failed"
+        });
+    })
+});
 module.exports = router;
