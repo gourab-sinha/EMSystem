9543d90656350324b09b3119af3037835557864d
gourab-sinha <gourab19964u@gmail.com>
2020-12-26 23:36:22 +0530

    login handler added

--- frontend/src/app/app-routing.model.ts
+++ frontend/src/app/app-routing.model.ts
@@ -1,12 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./auth/login/login.component";
 import { EmployeeCreateComponent } from "./employee/employee-create/employee-create.component";
 import { EmployeeListComponent } from "./employee/employee-list/employee-list.component";
 
 const routes: Routes = [
     { path: '', component: EmployeeListComponent},
     { path: 'create', component: EmployeeCreateComponent},
-    { path: 'edit/:employeeId', component: EmployeeCreateComponent}
+    { path: 'edit/:employeeId', component: EmployeeCreateComponent},
+    { path: 'login', component: LoginComponent }
 
 ];
 @NgModule({
